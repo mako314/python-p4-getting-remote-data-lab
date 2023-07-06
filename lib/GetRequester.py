482fb653adb68ef54301b1645e89a48b1eba29ef
Macolister <bispo.mac@gmail.com>
2023-07-06 09:59:13 -0400

    add json load

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        content_list = []
+        json_content = json.loads(self.get_response_body())
+        for content in json_content:
+            content_list.append(content)
+        return content_list
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
+
+print(get_requester.get_response_body())
